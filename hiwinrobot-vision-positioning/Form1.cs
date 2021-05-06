c2e1823111d2b34dea8b8bb4da9b93830b661e5b
ziteh <honmonoh@gmail.com>
2021-05-06 20:36:44 +0800

    Add connect code of arm and camera

--- hiwinrobot-vision-positioning/Form1.cs
+++ hiwinrobot-vision-positioning/Form1.cs
@@ -14,24 +14,34 @@ namespace hiwinrobot_vision_positioning
 {
     public partial class Form1 : Form
     {
+        private IMessage Message;
+        private IArmController Arm;
+        private IDSCamera Camera;
+        
         public Form1()
         {
             InitializeComponent();
+            
+            Message = new NormalMessage(new LogHandler());
+            Arm = new ArmController("192.168.0.3", Message);
+            Camera = new IDSCamera(Message);
         }
 
         private void buttonConnect_Click(object sender, EventArgs e)
         {
-
+            Arm.Connect();
+            Camera.Open();
         }
 
         private void buttonDisconnect_Click(object sender, EventArgs e)
         {
-
+            Arm.Disconnect();
+            Camera.Exit();
         }
 
         private void buttonHoming_Click(object sender, EventArgs e)
         {
-
+            Arm.Do(new Homing());
         }
     }
 }
