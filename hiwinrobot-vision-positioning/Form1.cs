12737b990563feebdbe8b7b1a4cd14cf0642b3d1
ziteh <honmonoh@gmail.com>
2021-05-06 19:12:34 +0800

    Add HRTK-NuGet Package

--- hiwinrobot-vision-positioning/Form1.cs
+++ hiwinrobot-vision-positioning/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NFUIRSL.HRTK;
+using NFUIRSL.HRTK.Vision;
 
 namespace hiwinrobot_vision_positioning
 {
