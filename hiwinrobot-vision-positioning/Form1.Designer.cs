47d33ecf895056ab4416dad24d7ec09c88e86e2f
ziteh <honmonoh@gmail.com>
2021-05-07 12:43:14 +0800

    Update UI

--- hiwinrobot-vision-positioning/Form1.Designer.cs
+++ hiwinrobot-vision-positioning/Form1.Designer.cs
@@ -34,20 +34,23 @@ namespace hiwinrobot_vision_positioning
             this.buttonDisconnect = new System.Windows.Forms.Button();
             this.buttonHoming = new System.Windows.Forms.Button();
             this.buttonStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBoxMain
             // 
-            this.pictureBoxMain.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxMain.Location = new System.Drawing.Point(226, 12);
             this.pictureBoxMain.Name = "pictureBoxMain";
-            this.pictureBoxMain.Size = new System.Drawing.Size(962, 641);
+            this.pictureBoxMain.Size = new System.Drawing.Size(1747, 1363);
+            this.pictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxMain.TabIndex = 0;
             this.pictureBoxMain.TabStop = false;
             // 
             // buttonConnect
             // 
-            this.buttonConnect.Location = new System.Drawing.Point(1074, 75);
+            this.buttonConnect.Location = new System.Drawing.Point(3, 3);
             this.buttonConnect.Name = "buttonConnect";
             this.buttonConnect.Size = new System.Drawing.Size(164, 67);
             this.buttonConnect.TabIndex = 1;
@@ -57,7 +60,7 @@ namespace hiwinrobot_vision_positioning
             // 
             // buttonDisconnect
             // 
-            this.buttonDisconnect.Location = new System.Drawing.Point(1074, 176);
+            this.buttonDisconnect.Location = new System.Drawing.Point(3, 76);
             this.buttonDisconnect.Name = "buttonDisconnect";
             this.buttonDisconnect.Size = new System.Drawing.Size(164, 67);
             this.buttonDisconnect.TabIndex = 1;
@@ -67,7 +70,8 @@ namespace hiwinrobot_vision_positioning
             // 
             // buttonHoming
             // 
-            this.buttonHoming.Location = new System.Drawing.Point(1074, 293);
+            this.buttonHoming.Enabled = false;
+            this.buttonHoming.Location = new System.Drawing.Point(3, 149);
             this.buttonHoming.Name = "buttonHoming";
             this.buttonHoming.Size = new System.Drawing.Size(164, 67);
             this.buttonHoming.TabIndex = 1;
@@ -78,7 +82,7 @@ namespace hiwinrobot_vision_positioning
             // buttonStart
             // 
             this.buttonStart.Enabled = false;
-            this.buttonStart.Location = new System.Drawing.Point(1074, 411);
+            this.buttonStart.Location = new System.Drawing.Point(3, 222);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(164, 67);
             this.buttonStart.TabIndex = 2;
@@ -86,19 +90,28 @@ namespace hiwinrobot_vision_positioning
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonConnect);
+            this.panel1.Controls.Add(this.buttonStart);
+            this.panel1.Controls.Add(this.buttonDisconnect);
+            this.panel1.Controls.Add(this.buttonHoming);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(208, 323);
+            this.panel1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1414, 1017);
-            this.Controls.Add(this.buttonStart);
-            this.Controls.Add(this.buttonHoming);
-            this.Controls.Add(this.buttonDisconnect);
-            this.Controls.Add(this.buttonConnect);
+            this.ClientSize = new System.Drawing.Size(1998, 1397);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBoxMain);
             this.Name = "Form1";
             this.Text = "Vision Positioning";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -110,6 +123,7 @@ namespace hiwinrobot_vision_positioning
         private System.Windows.Forms.Button buttonDisconnect;
         private System.Windows.Forms.Button buttonHoming;
         private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
